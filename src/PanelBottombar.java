e2c04344cd91ade7708bced1b4b8ba2f6721aa1b
Togohogo1 <59260907+Togohogo1@users.noreply.github.com>
2020-06-27 23:24:09 -0400

    Create PanelBottombar.java
    Didn't feel like creating the actual gui today lol. Did only the format as text.

---
+++ src/PanelBottombar.java
@@ -0,0 +1,7 @@
+// import java.awt.*;
+// import java.awt.event.*;
+// import javax.swing.*;
+
+// public class PanelBottombar extends JPanel implements ActionListener {
+//     |Mouse: 100px, 100px    |Canvas: 800px by 600px    |Current Tool: Pen Tool    |Stroke Width: 12px    |Stroke Color: #FFFFFF    |
+// }
